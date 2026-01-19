a2856d5d71051e04cc0f362a09a132ccf5f30de6
Jason-Madsen <mad22015@byui.edu>
2026-01-19 09:55:19 -0700

    Updated H3 to H2

--- mission/mission.js
+++ mission/mission.js
@@ -13,7 +13,7 @@ function changeTheme() {
         document.querySelector('#how').classList.add('darkmode');
         document.querySelector('#list').classList.add('darkmode');
         document.querySelector('img').setAttribute('src', 'images/byui-logo-white.png');
-        document.querySelector('h3').style.color = "#87AFC7";
+        document.querySelector('h2').style.color = "#87AFC7";
         document.querySelector('body').style.borderColor = 'white';
     } else {
         document.body.style.backgroundColor = 'white';
@@ -22,7 +22,7 @@ function changeTheme() {
         document.querySelector('#how').classList.remove('darkmode');
         document.querySelector('#list').classList.remove('darkmode');
         document.querySelector('img').setAttribute('src', 'images/byui-logo.webp');
-        document.querySelector('h3').style.color = "#3289cc";
+        document.querySelector('h2').style.color = "#3289cc";
         document.querySelector('body').style.borderColor = 'black';
     }
 }           
