f5130aeef854d4db7990ca005a87e286ea333362
Jason-Madsen <mad22015@byui.edu>
2026-01-19 10:19:31 -0700

    js improvements

--- mission/mission.js
+++ mission/mission.js
@@ -1,28 +1,34 @@
 
 let selectElem = document.querySelector('select');
 let logo = document.querySelector('img');
-let image = selectElem.addEventListener('change', changeTheme);
+let body = document.body;
+let h1 = document.querySelector('h1');
+let h2 = document.querySelector('h2');
+let statement = document.querySelector('#statement');
+let how = document.querySelector('#how');
+let list = document.querySelector('#list');
+selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
-        document.body.style.backgroundColor = '#282828';
-        document.querySelector('h1').classList.add('darkmode');
-        document.querySelector('#statement').classList.add('darkmode');
-        document.querySelector('#how').classList.add('darkmode');
-        document.querySelector('#list').classList.add('darkmode');
-        document.querySelector('img').setAttribute('src', 'images/byui-logo-white.png');
-        document.querySelector('h2').style.color = "#87AFC7";
-        document.querySelector('body').style.borderColor = 'white';
+        body.style.backgroundColor = '#282828';
+        h1.classList.add('darkmode');
+        statement.classList.add('darkmode');
+        how.classList.add('darkmode');
+        list.classList.add('darkmode');
+        logo.setAttribute('src', 'images/byui-logo-white.png');
+        h2.style.color = "#87AFC7";
+        body.style.borderColor = 'white';
     } else {
-        document.body.style.backgroundColor = 'white';
-        document.querySelector('h1').classList.remove('darkmode');
-        document.querySelector('#statement').classList.remove('darkmode');
-        document.querySelector('#how').classList.remove('darkmode');
-        document.querySelector('#list').classList.remove('darkmode');
-        document.querySelector('img').setAttribute('src', 'images/byui-logo.webp');
-        document.querySelector('h2').style.color = "#3289cc";
-        document.querySelector('body').style.borderColor = 'black';
+        body.style.backgroundColor = 'white';
+        h1.classList.remove('darkmode');
+        statement.classList.remove('darkmode');
+        how.classList.remove('darkmode');
+        list.classList.remove('darkmode');
+        logo.setAttribute('src', 'images/byui-logo.webp');
+        h2.style.color = "#3289cc";
+        body.style.borderColor = 'black';
     }
 }           
                     
